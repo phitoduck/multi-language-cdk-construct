088d146b2faad934ee2e28b3de9bb9c7de63795c
Eric Riddoch <eric.riddoch@bengroup.com>
2022-08-26 14:49:39 -0600

    saving changes

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,28 @@
 const { awscdk } = require('projen');
+
+const CDK_VERSION = "2.39.0"
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Eric Riddoch',
   authorAddress: 'eric.riddoch@bengroup.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: CDK_VERSION,
   defaultReleaseBranch: 'main',
   name: 'multi-language-construct',
   repositoryUrl: 'https://github.com/eric.riddoch/multi-language-construct.git',
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  deps: [
+    // `@aws-cdk/aws-apigatewayv2-alpha@${CDK_VERSION}`,
+    `@aws-cdk/aws-apigatewayv2-alpha`,
+    `@aws-cdk/aws-apigatewayv2-integrations-alpha`,
+  ],                /* Runtime dependencies of this module. */
+  description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  devDeps: [],             /* Build dependencies for this module. */
+  packageName: "multi-language-construct-eric",  /* The "name" in package.json. */
+  publishDryRun: true,
+  publishToPypi:{
+    distName: "multi-language-construct-eric",
+    module: "multi_language_construct_eric",
+  },
+  stability: "experimental" // this value is arbitrary
 });
 project.synth();
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# replace this
+# Eric's first multi-language construct
 
 This project was created using
 
@@ -6,4 +6,20 @@ This project was created using
 mkdir multi-language-construct
 npm -g install yarn
 npx projen new awscdk-construct
+```
+
+Then I edited values in `.projenrc.js` using autocompletion. I set items such as project
+dependencies and the fact that I want this to be ported to python.
+
+```bash
+# update the projen project to reflect the configurations in .projenrc.js
+npx projen
+```
+
+I filled out a basic construct, then I wired the project up to be a valid CDK project:
+
+```bash
+mkdir ./bin
+touch ./bin/app.ts
+# ... filled out this file based on the boilerplate typescript template
 ```
